6292f39d0058e5aaefdf7411982061ec93b1ce45
jotone <hereyouare1987@gmail.com>
2017-05-22 15:59:33 +0300

    view payment page

--- app/Payment.php
+++ app/Payment.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 class Payment extends Model{
     protected $table = 'tbl_payment';
     protected $fillable = [
-        'user_id', 'money_amount', 'gold_amount', 'pay_status', 'type'
+        'user_id','user_name', 'money_amount', 'gold_amount', 'pay_status', 'type', 'last_gold_status','last_exchange_status'
     ];
 }
--- resources/views/admin/payments.blade.php
+++ resources/views/admin/payments.blade.php
@@ -1,35 +1,74 @@
 @extends('admin.layouts.default')
 @section('content')
 
-    <div class="main-central-wrap">
-        <table class="data-table">
-            <thead>
-            <tr>
-                <th></th>
-                <th></th>
+	<div class="main-central-wrap">
+		<table class="data-table">
+			<thead>
+			<tr>
+				<th>
+					<span>Пользователь</span>
+					<div class="tac">
+						<a class="table-direction" href="#" data-direct="up">&#9650;</a>
+						<a class="table-direction" href="#" data-direct="down">&#9660;</a>
+					</div>
+				</th>
+				<th>
+					<span>Получено денег (без учета комиссии)</span>
+					<div class="tac">
+						<a class="table-direction" href="#" data-direct="up">&#9650;</a>
+						<a class="table-direction" href="#" data-direct="down">&#9660;</a>
+					</div>
+				</th>
+				<th>
+					<span>Кол-во золота до оплаты</span>
+					<div class="tac">
+						<a class="table-direction" href="#" data-direct="up">&#9650;</a>
+						<a class="table-direction" href="#" data-direct="down">&#9660;</a>
+					</div>
+				</th>
                 <th>
-                    <span>Пользователь</span>
-                    <div style="text-align: center;">
-                        <a class="table-direction" href="#" data-direct="up">&#9650;</a>
-                        <a class="table-direction active" href="#" data-direct="down">&#9660;</a>
-                    </div>
-                </th>
-                <th>
-                    <span>Создан</span>
-                    <div style="text-align: center;">
+                    <span>Получено золота</span>
+                    <div class="tac">
                         <a class="table-direction" href="#" data-direct="up">&#9650;</a>
                         <a class="table-direction" href="#" data-direct="down">&#9660;</a>
                     </div>
                 </th>
-                <th>
-                    <span>Оплачен</span>
-                    <div style="text-align: center;">
-                        <a class="table-direction" href="#" data-direct="up">&#9650;</a>
-                        <a class="table-direction" href="#" data-direct="down">&#9660;</a>
-                    </div>
-                </th>
-            </tr>
-            </thead>
-        </table>
-    </div>
-    @stop
+				<th>
+					<span>Оплата по курсу</span>
+					<div class="tac">
+						<a class="table-direction" href="#" data-direct="up">&#9650;</a>
+						<a class="table-direction" href="#" data-direct="down">&#9660;</a>
+					</div>
+				</th>
+				<th>
+					<span>Создан</span>
+					<div class="tac">
+						<a class="table-direction" href="#" data-direct="up">&#9650;</a>
+						<a class="table-direction" href="#" data-direct="down">&#9660;</a>
+					</div>
+				</th>
+				<th>
+					<span>Оплачен</span>
+					<div class="tac">
+						<a class="table-direction" href="#" data-direct="up">&#9650;</a>
+						<a class="table-direction active" href="#" data-direct="down">&#9660;</a>
+					</div>
+				</th>
+			</tr>
+			</thead>
+            <tbody>
+            @foreach($payments as $payment)
+                <tr>
+                    <td><a href="{{ route('admin-user-edit-page', $payment->user_id) }}">{{ $payment->user_name }}</a></td>
+                    <td>{{ $payment->money_amount }} руб.</td>
+                    <td>{{ $payment->last_gold_status }}</td>
+                    <td>{{ $payment->gold_amount }}</td>
+                    <td>{{ $payment->last_exchange_status }} руб. за доллар</td>
+                    <td>{{ substr($payment->created_at,0,16) }}</td>
+                    <td>{{ substr($payment->updated_at,0,16) }}</td>
+                </tr>
+            @endforeach
+            </tbody>
+		</table>
+	</div>
+	@stop
