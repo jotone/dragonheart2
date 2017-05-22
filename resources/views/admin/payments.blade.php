45cfaa4101fb0c581103951d13176ee1fdce7d0f
jotone <hereyouare1987@gmail.com>
2017-05-22 16:14:42 +0300

    show users payment in @user view@

--- resources/views/admin/payments.blade.php
+++ resources/views/admin/payments.blade.php
@@ -26,13 +26,13 @@
 						<a class="table-direction" href="#" data-direct="down">&#9660;</a>
 					</div>
 				</th>
-                <th>
-                    <span>Получено золота</span>
-                    <div class="tac">
-                        <a class="table-direction" href="#" data-direct="up">&#9650;</a>
-                        <a class="table-direction" href="#" data-direct="down">&#9660;</a>
-                    </div>
-                </th>
+				<th>
+					<span>Получено золота</span>
+					<div class="tac">
+						<a class="table-direction" href="#" data-direct="up">&#9650;</a>
+						<a class="table-direction" href="#" data-direct="down">&#9660;</a>
+					</div>
+				</th>
 				<th>
 					<span>Оплата по курсу</span>
 					<div class="tac">
@@ -48,27 +48,27 @@
 					</div>
 				</th>
 				<th>
-					<span>Оплачен</span>
+					<span>Дата оплаты</span>
 					<div class="tac">
-						<a class="table-direction" href="#" data-direct="up">&#9650;</a>
-						<a class="table-direction active" href="#" data-direct="down">&#9660;</a>
+						<a class="table-direction active" href="#" data-direct="up">&#9650;</a>
+						<a class="table-direction" href="#" data-direct="down">&#9660;</a>
 					</div>
 				</th>
 			</tr>
 			</thead>
-            <tbody>
-            @foreach($payments as $payment)
-                <tr>
-                    <td><a href="{{ route('admin-user-edit-page', $payment->user_id) }}">{{ $payment->user_name }}</a></td>
-                    <td>{{ $payment->money_amount }} руб.</td>
-                    <td>{{ $payment->last_gold_status }}</td>
-                    <td>{{ $payment->gold_amount }}</td>
-                    <td>{{ $payment->last_exchange_status }} руб. за доллар</td>
-                    <td>{{ substr($payment->created_at,0,16) }}</td>
-                    <td>{{ substr($payment->updated_at,0,16) }}</td>
-                </tr>
-            @endforeach
-            </tbody>
+			<tbody>
+			@foreach($payments as $payment)
+				<tr>
+					<td><a href="{{ route('admin-user-edit-page', $payment->user_id) }}">{{ $payment->user_name }}</a></td>
+					<td>{{ $payment->money_amount }} руб.</td>
+					<td>{{ $payment->last_gold_status }}</td>
+					<td>{{ $payment->gold_amount }}</td>
+					<td>{{ $payment->last_exchange_status }} руб. за доллар</td>
+					<td>{{ substr($payment->created_at,0,16) }}</td>
+					<td>{{ substr($payment->updated_at,0,16) }}</td>
+				</tr>
+			@endforeach
+			</tbody>
 		</table>
 	</div>
 	@stop
